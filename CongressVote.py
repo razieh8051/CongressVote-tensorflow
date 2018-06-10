263794fc3ac2295379085b23686bab2d8d95ef90
Razieh <razieh@Raziehs-MacBook-Pro-3.local>
2018-06-10 08:51:31 -0400

    Second commit

--- CongressVote.py
+++ CongressVote.py
@@ -1,48 +1,41 @@
 import numpy as np
 import pandas as pd
 import tensorflow as tf
+import os.path
 
 
 
-def create_model(inputs,nfeatures,nclasses):
+def create_model(inputs, weights, biases):
 
-    hidden_nodes_layer1=10
-    hidden_nodes_layer2=10
-
-    input_weights = tf.Variable(tf.truncated_normal([nfeatures, hidden_nodes_layer1]))
-    input_biases = tf.Variable(tf.zeros([1,hidden_nodes_layer1]))
-
-    hidden1_weights = tf.Variable(tf.truncated_normal([hidden_nodes_layer1, hidden_nodes_layer2]))
-    hidden1_biases = tf.Variable(tf.zeros([1,hidden_nodes_layer2]))
-
-    hidden2_weights = tf.Variable(tf.truncated_normal([hidden_nodes_layer2, nclasses]))
-    hidden2_biases = tf.Variable(tf.zeros([1,nclasses]))
 
     #Create 2-layered NN
-    input_layer= tf.matmul(inputs,input_weights)
-    hidden_layer1= tf.nn.relu(input_layer + input_biases)
-
-    hidden_layer2= tf.nn.relu(tf.matmul(hidden_layer1,hidden1_weights)+hidden1_biases)
-    output_layer= tf.matmul(hidden_layer2, hidden2_weights) + hidden2_biases
+    input_layer= tf.matmul(inputs,weights['input'])
+    # Hidden layer with RELU activation
+    hidden_layer1= tf.nn.relu(input_layer + biases['input'])
+    # Hidden layer with RELU activation
+    hidden_layer2= tf.nn.relu(tf.matmul(hidden_layer1,weights['hidden1'])+biases['hidden1'])
+    # Output layer with linear activation
+    output_layer= tf.matmul(hidden_layer2, weights['hidden2']) + biases['hidden2']
 
     return output_layer
 
+#First Column is the label and others are the features
 COLUMNS = ['className','handicapped-infants','water-project-cost-sharing','adoption-of-the-budget-resolution',
 'physician-fee-freeze','el-salvador-aid','religious-groups-in-schools','anti-satellite-test-ban',
 'aid-to-nicaraguan-contras','mx-missle','immigration','synfuels-corporation-cutback','education-spending',
 'superfund-right-to-sue','crime','duty-free-exports','export-administration-act-south-africa']
 
-#read data into CSV file and convert ? to NaN
-df = pd.read_csv('house-votes-84.data.txt'
+# Read data into CSV file and convert ? to NaN
+df = pd.read_csv(os.path.join(os.path.dirname(__file__), "./house-votes-84.data.txt")
                        , names=COLUMNS
                        , skipinitialspace=True
                        , na_values="?")
-#label column republican/democrat to 1 and 0
+#Label column republican/democrat to 1 and 0
 for i in range(0,len(COLUMNS)):
     cleanup_nums = {COLUMNS[i]: {"y":1, "n":0, "republican":1,"democrat":0}}
     df.replace(cleanup_nums, inplace=True)
 
-#label=np.array(df[0])
+#Make 2 one-hot vectors for labels columns
 lables=np.zeros((435, 2))
 label=df['className']
 for i in range(0,len(label)):
@@ -52,24 +45,47 @@ for i in range(0,len(label)):
     else:
         lables[i,0]=1
         lables[i,1]=0
-print(lables)
 inputs=df[COLUMNS[2:]]
 inputs=inputs.values
-print inputs
-print inputs.shape
 
-n_calsses= 2
+#HyperParameters Training
+learning_rate = 0.5
+epochs = 100
+batch_size = 100
+display_step = 1
+
 n_features = 15
+n_calsses=2
+
+#HyperParameters NN
+hidden_nodes_layer1=10
+hidden_nodes_layer2=10
+
+#Define weights and biases
+input_weights = tf.Variable(tf.random_normal([n_features, hidden_nodes_layer1]))
+input_biases = tf.Variable(tf.zeros([1,hidden_nodes_layer1]))
+
+hidden1_weights = tf.Variable(tf.random_normal([hidden_nodes_layer1, hidden_nodes_layer2]))
+hidden1_biases = tf.Variable(tf.zeros([1,hidden_nodes_layer2]))
+
+hidden2_weights = tf.Variable(tf.random_normal([hidden_nodes_layer2, n_calsses]))
+hidden2_biases = tf.Variable(tf.zeros([1,n_calsses]))
+
+weights={'input':input_weights,'hidden1':hidden1_weights,'hidden2':hidden2_weights}
+biases={'input':input_biases,'hidden1':hidden1_biases,'hidden2':hidden2_biases}
+
+
 x = tf.placeholder(tf.float32, [None, n_features])
 y = tf.placeholder(tf.float32, [None, n_calsses])
 
-prediction=create_model(x,n_features,n_calsses)
+#Create the NN model
+prediction=create_model(x,weights,biases)
 
 loss_function = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=y))
 
-optimizer = tf.train.GradientDescentOptimizer(0.5).minimize(loss_function)
+optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss_function)
 
-
+ 
 sess = tf.Session()
 init = tf.global_variables_initializer()
 sess.run(init)
@@ -78,13 +94,9 @@ batch_size = 1
 
 with tf.Session() as sess:
     sess.run(init)    
-    # Training cycle
-    for epoch in range(201):
+    #Training cycle
+    for epoch in range(epochs):
         optimizer.run(feed_dict={x: inputs, y: lables})
         if ((x+1) % 100 == 0):
             print("Training epoch " + str(x+1))
-            print("Accuracy: " + str(optimizer.run(feed_dict={x: inputs, y: lables})
-  
-
-        # step = sess.run(optimizer, feed_dict={x: inputs, y: lables})
-        # print(step)
+            print("Accuracy: " + str(optimizer.run(feed_dict={x: inputs, y: lables})))
